d0ce303d970aac943cfe6fb881ae1779301121e2
peleg122 <peleg122@gmail.com>
2017-03-02 18:01:16 +0200

    Create Restaurant.java

---
+++ Restaurant.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class Restaurant {
+	public static void main(String[] args) {
+    
+    HashMap<String, Integer> restaurantMenu = new HashMap<String, Integer>();
+    
+
+
+	}
+}
